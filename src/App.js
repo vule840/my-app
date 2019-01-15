4002ca5625f4e9cd23f7ac0db3ffc79d70432f6b
vule840 <vule840@gmail.com>
2019-01-15 22:28:27 +0100

    1

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+	
+	state = {
+        ip_adress: '...'
+        
+
+  }
+
+	componentDidMount(){
+		this.fetchData()
+
+	}
+
+	fetchData(){
+		fetch('https://jsonplaceholder.typicode.com/todos/1')
+		  .then(response => response.json())
+		  .then(data => console.log(data))
+		  .catch(error => console.log('failed',error))
+
+		  	
+	}
+	
+
+   render() {
+
+   	var {isLoading, contacts} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+     sdfsdf
       </div>
     );
   }
