ff5407ec102ba959ba1749d0d2a1532393622ca5
vule840 <vule840@gmail.com>
2019-01-17 16:47:26 +0100

    3

--- src/App.js
+++ src/App.js
@@ -1,61 +1,98 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
+import Card from "./components/Card";
+import SimpleMap from "./components/SimpleMap";
 
+//importaš ikone i onda ih koristiš kao komponennte
+import {
+	FaUser,
+	FaCity,
+	FaEnvelope,
+	FaGenderless,
+	FaMapMarker
+} from "react-icons/fa";
 
-import Card from "./components/Card"
-
-import { FaBeer, FaUser, FaCity } from 'react-icons/fa';
+//https://github.com/google-map-react/google-map-react
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			items: [],
-			isLoaded: false
+			isLoaded: false,
+			center: {
+				lat: 59.95,
+				lng: 30.33
+			},
+			zoom: 11
 		};
-	}  
+	}
 	componentDidMount() {
 		fetch("https://randomuser.me/api/?results=10")
 			.then(res => res.json())
 			.then(json => {
 				this.setState({
 					isLoaded: true,
-					items: json
+					items: json,
+					center: {
+						lat: 59.95,
+						lng: 30.33
+					},
+					zoom: 11
 				});
 				console.log(this.state.items);
 			});
 	}
 	render() {
-		var { isLoaded, items } = this.state;
+		var { isLoaded, items, center } = this.state;
 		if (!isLoaded) {
 			return <div>Loading...</div>;
 		}
 		return (
 			<div className="App">
 				<div className="container">
-				  	<div className="row">
-					{items.results.map(item => (
-						<div className="col-4 p-3">
-							<div className="card bg-dark text-white">
-							 <img className="card-img-top" src={item.picture.large} />
-							  <div className="card-body">
-							    <h5 className="card-title"><FaUser /> {item.name.first}</h5>
-							    <h5 className="card-title"><FaCity /> {item.location.city}</h5>
-							    <p className="card-text"> {item.email}</p>
-							    <p className="card-text">{item.gender}</p>
-							    
-							    <a href="#" className="btn btn-primary">Go somewhere</a>
-							  </div>
+					<div className="row">
+						{items.results.map(item => (
+							<div className="col p-3 ">
+								<div className="card bg-dark text-white">
+									<img
+										className="card-img-top"
+										src={item.picture.large}
+									/>
+									<div className="card-body">
+										<p className="card-title">
+											<FaUser /> {item.name.title}.{" "}
+											{item.name.first}
+											{item.location.coordinates.latitude}
+										</p>
+										<p className="card-title">
+											<FaCity /> {item.location.city}
+										</p>
+										<p className="card-text smaller">
+											<FaEnvelope /> {item.email}
+										</p>
+										<p className="card-text">
+											<FaGenderless /> {item.gender}
+										</p>
+
+										<p className="card-text">
+											<FaMapMarker />{" "}
+											{item.location.state}
+										</p>
+
+										{/*<a href="#" className="btn btn-primary">
+											Go somewhere
+										</a>*/}
+									</div>
+								</div>
+								<SimpleMap />
 							</div>
-						</div>
-						
-					))}
+						))}
 					</div>
 				</div>
-				
-
 
 				{/*<ul>
 					{items.results.map(item => (
@@ -67,8 +104,6 @@ class App extends Component {
 
 
 				<Card></Card>*/}
-
-
 			</div>
 		);
 	}
---
+++ src/components/SimpleMap.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
+class SimpleMap extends Component {
+	static defaultProps = {
+		center: {
+			lat: 59.95,
+			lng: 30.33
+		},
+		zoom: 11
+	};
+
+	render() {
+		return (
+			// Important! Always set the container height explicitly
+			<div style={{ height: "20vh", width: "100%" }}>
+				<GoogleMapReact
+					yesIWantToUseGoogleMapApiInternals
+					bootstrapURLKeys={{
+						key: "AIzaSyBFs8_ddE2XezCLmjxxfvQACL7g2-J2t8o"
+					}}
+					defaultCenter={this.props.center}
+					defaultZoom={this.props.zoom}
+				/>
+				{/* <AnyReactComponent
+					lat={59.955413}
+					lng={30.337844}
+					text={"Kreyser Avrora"}
+				/> */}
+			</div>
+		);
+	}
+}
+
+export default SimpleMap;
